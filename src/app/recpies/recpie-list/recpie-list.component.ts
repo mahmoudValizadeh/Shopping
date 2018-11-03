fa46ce00154ec9bc1336ec4ea20d8fdde36093f1
mahmoudValizadeh <44633242+mahmoudValizadeh@users.noreply.github.com>
2018-11-03 17:58:02 +0330

    recipes
    انجام قسمتی از افزودن

--- src/app/recpies/recpie-list/recpie-list.component.ts
+++ src/app/recpies/recpie-list/recpie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {recpie} from '../recpie.model';
 
 @Component({
   selector: 'app-recpie-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recpie-list.component.css']
 })
 export class RecpieListComponent implements OnInit {
-
+recpies:recpie[]=[
+    new recpie('دستور پخت ماکارانی','دستور پخت تست برای ماکارانی','http://media.jamejamonline.ir/Media/Image/1394/06/10/635766990327000778.jpg'),
+    new recpie('دستور پخت ماکارانی','دستور پخت تست برای ماکارانی','http://media.jamejamonline.ir/Media/Image/1394/06/10/635766990327000778.jpg'),
+    new recpie('دستور پخت ماکارانی','دستور پخت تست برای ماکارانی','http://media.jamejamonline.ir/Media/Image/1394/06/10/635766990327000778.jpg'),
+];
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/recpies/recpie.model.ts
@@ -0,0 +1,11 @@
+export class recpie {
+    name:string;
+    description :string;
+    imagePath:string;
+    constructor(name:string,description:string,imagePath:string){
+        this.name=name;
+        this.description=description;
+        this.imagePath=imagePath;
+    }
+
+}
